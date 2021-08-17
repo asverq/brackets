5662720b19e4535280a4250d16fbf8246a95b77a
Igor Arinichev <igor.arin@gmail.com>
2021-08-18 00:32:01 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const strArr = str.split('')
+    const stack = [];
+    const BRACKETS_PAIR = {};
+    bracketsConfig.forEach((item) => BRACKETS_PAIR[item[0]] = item[1]);
+
+    for (let i = 0; i < strArr.length; i++) {
+        const currSym = strArr[i];
+        const stackHead = stack[stack.length - 1];
+        if (BRACKETS_PAIR[stackHead] === currSym) {
+            stack.pop();
+        } else if (BRACKETS_PAIR[currSym]) {
+            stack.push(currSym);
+        } else {
+            return false;
+        }
+    }
+
+    return stack.length === 0;
 }
